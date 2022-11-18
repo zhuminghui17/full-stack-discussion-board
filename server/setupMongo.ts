9dfea961f771948de108f99b4d62d45cc26d06f6
zhuminghui17 <zhuminghui17@gmail.com>
2022-11-18 11:48:10 -0500

    add setupMongo

---
+++ server/setupMongo.ts
@@ -0,0 +1,78 @@
+import { MongoClient, ObjectId } from 'mongodb'
+import { Post, Comment, User, Group } from './data'
+
+// Connection URL
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+
+const posts: Post[] = [
+  {
+    _id: 'p1', // post id
+    user_id: 'u1',
+    group_id: 'g1',
+    post_title: 'Hello World!',
+    post_content: 'Hello World! Hello World!' ,
+    timeStamp: Date, 
+    comment: [], // comment
+    upvote: 0,
+    downvote: 0 
+  },
+  {
+    _id: "p2",
+    user_id : 'u2',
+    group_id: 'g2',
+    post_title: 'Second post!',
+    post_content: 'This is the second post' ,
+    timeStamp: Date,
+    comment: [], // comment
+    upvote: 10,
+    downvote: 100, 
+  },
+]
+
+const comments: Comment[] = [
+    {
+      _id: 'c1', 
+      comment_content: 'this is the first comment', 
+      timeStamp: Date, 
+      upvote: 0,
+      downvote: 0 
+    },
+  ]
+
+const users: User[] = [
+    {
+      _id: 'c1', 
+      name: 'ccdd'
+    },
+  ]
+
+const groups: Group[] = [
+    {
+      _id: 'g1', 
+      name: 'Example Group'
+    },
+  ]
+
+
+async function main() {
+  await client.connect()
+  console.log('Connected successfully to MongoDB')
+
+  const db = client.db("test")
+
+  // set up unique index for upsert -- to make sure a customer cannot have more than one draft order
+  db.collection("orders").createIndex(
+    { customerId: 1 }, 
+    { unique: true, partialFilterExpression: { state: "draft" } }
+  )
+
+  // add data
+  console.log("inserting posts", await db.collection("posts").insertMany(posts as any))
+  console.log("inserting comments", await db.collection("posts").insertMany(comments as any))
+  console.log("inserting users", await db.collection("users").insertMany(users as any))
+  console.log("inserting groups", await db.collection("groups").insertMany(groups as any))
+  process.exit(0)
+}
+
+main()
