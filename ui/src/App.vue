7ce2449681f94c0035d07a99dd9e1b9ec1527388
Congcong Ma <cm645@duke.edu>
2022-11-18 11:02:54 -0500

    A demo of UI

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="primary">
-      <b-navbar-brand href="#">This is my discussion board</b-navbar-brand>      
+      <b-navbar-brand href="#">This is my discussion board</b-navbar-brand>
     </b-navbar>
 
     <b-container fluid class="my-4">
@@ -12,17 +12,17 @@
         <b-col xs="12" sm="3">
 
           <b-list-group flush>
-            <b-list-group-item>
-              10086
+
+            <b-list-group-item button>
+              <span @click="alert">10086</span>
             </b-list-group-item>
 
-            <span @click="alert">1</span>
 
-            <b-list-group-item>
-              10086
+            <b-list-group-item button>
+              <span @click="alert">10086</span>
             </b-list-group-item>
-            <b-list-group-item>
-              10086
+            <b-list-group-item button>
+              <span @click="alert">10086</span>
             </b-list-group-item>
           </b-list-group>
 
@@ -32,15 +32,66 @@
           <b-card>
             10086
           </b-card>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <b-button class = "my-3">Post</b-button>
+          </div>
         </b-col>
       </b-row>
     </b-container>
-    
+
   </div>
 </template>
 
 <script setup lang="ts">
-function alert(){
-  alert(10086)
+
+export interface User {
+    _id: string,
+    name: string
 }
+
+export interface Post {
+    _id: string, 
+    author_id: string, 
+    post_title: string,
+    time: string,
+    post_content: string 
+}
+
+
+
+
+
+
+
+
+function alert() {
+  console.log("10086")
+}
+
+const post1:Post = {
+  _id: "A",
+  author_id: "CCDD",
+  post_title: "This is a demo",
+  time: "2012",
+  post_content: "I say, this is a demo"
+}
+
+const post2:Post = {
+  _id: "B",
+  author_id: "DDCC",
+  post_title: "This is a demo2",
+  time: "2013",
+  post_content: "I say, this is a demo2"
+}
+
+const posts:Post[] = [post1, post2]
+// TODO:
+// 1. 调整页面样式，固定比例
+// 2. 显示相应内容
+// 3. 完成按钮操作
+
+
+
 </script>
