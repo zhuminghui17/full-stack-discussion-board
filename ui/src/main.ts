bfebd914d6de2d76185c5470cd9abfdec864ca25
Congcong Ma <cm645@duke.edu>
2022-11-26 13:43:45 -0500

    Add routers to the file

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import App from '@/App.vue'
+import VueRouter from 'vue-router'
+import App from '@/views/App.vue'
 
+import studentScreen from "@/views/studentScreen.vue"
+import professorScreen from "@/views/professorScreen.vue"
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue"
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -8,9 +11,28 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(VueRouter)
 
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/student",
+      component: studentScreen
+    },
+    {
+      path:"/professor",
+      component: professorScreen
+    }
+  ],
+})
 Vue.config.productionTip = false
 Vue.config.devtools = true
+
+
+
+
 
 /* eslint-disable no-new */
 new Vue({
