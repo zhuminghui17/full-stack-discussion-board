f15ec69b3494f26f710cf4306c14d919dc5d26a0
zhuminghui17 <zhuminghui17@gmail.com>
2022-11-27 03:42:31 -0500

    keycloak step 2

--- server/secrets.ts
+++ server/secrets.ts
@@ -1,5 +1,5 @@
 export const keycloak = {
-  client_id: "smoothie",
+  client_id: "discussion",
   client_secret: "", // TODO
   redirect_uris: ["http://127.0.0.1:8080/api/login-callback"],
   post_logout_redirect_uris: [""],
