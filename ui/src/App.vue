f68dc7af19d88c2b2c74733277407e12943bab2d
zhuminghui17 <zhuminghui17@gmail.com>
2022-11-28 17:48:10 -0500

    delete navi bar

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,12 +1,5 @@
 <template>
     <div>
-        <b-navbar-nav>
-        <b-nav-item v-if="user?.roles?.includes('student')" href="/student">I want to go to student's screen</b-nav-item>
-        <b-nav-item v-if="user?.roles?.includes('professor')" href="/professor">I want to go to professor </b-nav-item>
-        <!-- <b-nav-item v-if="user?.name == null" href="/api/login">Login</b-nav-item>
-        <b-nav-item v-if="user?.name" @click="logout">Logout</b-nav-item>
-        <form method="POST" action="/api/logout" id="logoutForm" /> -->
-      </b-navbar-nav>
     <router-view/>
     </div>
 </template>
