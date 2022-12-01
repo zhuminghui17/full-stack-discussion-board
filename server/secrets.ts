768cb1ae6107158da9d04a469196152dce523f5d
Congcong Ma <cm645@duke.edu>
2022-11-30 22:57:03 -0500

    Add secret into git

---
+++ server/secrets.ts
@@ -0,0 +1,8 @@
+export const keycloak = {
+    client_id: "discussion",
+    client_secret: "15s8xIWx9EgEVYZfWbeKjaxrz9ImWlz6", // TODO
+    redirect_uris: ["http://127.0.0.1:8080/api/login-callback"],
+    post_logout_redirect_uris: [""],
+    response_types: ["code"],
+  }
+  
