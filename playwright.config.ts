262125ade3c863f5d5810e3036ac7d3575ad8809
zhuminghui17 <zhuminghui17@gmail.com>
2022-12-02 15:13:40 -0500

    should fail the test faster

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 30 * 300,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 500
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
