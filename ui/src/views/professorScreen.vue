12a3f2562bbd8a9ee110331316e8c9124bedc5f5
zhuminghui17 <zhuminghui17@gmail.com>
2022-12-02 12:11:01 -0500

    update ui

--- ui/src/views/professorScreen.vue
+++ ui/src/views/professorScreen.vue
@@ -63,8 +63,10 @@
                 </b-col>
                 <!-- The second column consists of functionality that professor can invite students  -->
                 <b-col xs="12" sm="9">
-                    <b-card-group deck>
-                        <b-card v-for="group, i in groupsInfo" :key="i" bg-variant="light" text-variant="black"
+                    <b-card-group columns>
+                        <b-card
+                        img-alt="Group" img-top 
+                        v-for="group, i in groupsInfo" :img-src="imgSrcs[i]" :key="i" bg-variant="light" text-variant="black"
                             :title=group._id>
                             <b-card-text>
                                 {{ group.name }}
@@ -161,6 +163,37 @@ async function AddNewGroup() {
 function logout() {
   ;(window.document.getElementById('logoutForm') as HTMLFormElement).submit()  
 }
+
+const imgSrcs:string[] = [
+"https://cdn.dribbble.com/users/2367860/screenshots/16207023/media/2ba160192820f5455364dc7ab4a0a09a.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15379124/media/546c9da2cdb213b66b41c2a2cb4cbdf0.png?compress=1&resize=800x600&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15394564/media/36e77c41759263879d9c797d0b58e929.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15405830/media/57fd5f629e2f82839fb6657ceb203d65.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15420005/media/830161c2fc41a68c5e189e4cb79ba71a.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/16693585/media/1727ffeefbda22e2278f0362be55e460.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/14614248/media/36fe29031a0d78e678361a7a3952926f.png?compress=1&resize=800x600&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15167941/media/35d39a36a96c654c0ec4c797a48cd8db.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15456940/media/7ff3b69ed1e4731ca7dda1c312b55829.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15608655/media/2324c1272d4984218992d82e51937311.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/16207023/media/2ba160192820f5455364dc7ab4a0a09a.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/16813804/media/3b9e5712f31488f3df3cb4f91a3076e6.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/16836865/media/1805a04e24b0312bd6d312539dc58e3d.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/15237967/media/912e3b854d9d1e4758747b945c5c9bf2.png?compress=1&resize=800x600&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/14999260/media/af1f9c4e75da35b6a9e35079756d70ed.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/17753379/media/45886ae44312527252531a65fda17357.png?compress=1&resize=1600x1200&vertical=top",
+"https://cdn.dribbble.com/users/2367860/screenshots/16359632/media/94d1cc333602ec8a7786962a1dd8c609.png?compress=1&resize=1600x1200&vertical=top",
+
+
+]
+
+// let imgsrcIndexs:number[] = []
+// function randomSelectImgSrc() {
+//     console.log('10086')
+//     let count:number = imgSrcs.length
+//     const choice:number = Math.floor(Math.random() * count)
+//     imgsrcIndexs.push(choice)
+//     return imgSrcs[choice]
+// }
 
 </script>
 
